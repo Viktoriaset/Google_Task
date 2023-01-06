2fc0b678b6a89cdfbc681690b49d0fd6a6387488
ViktoriTanos <99451694+Viktoriaset@users.noreply.github.com>
2023-01-06 12:00:52 +1000

    created list container and added button favorites, my task and create list

--- app/src/main/java/com/example/google_task/MainActivity.kt
+++ app/src/main/java/com/example/google_task/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.google_task
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
+import android.widget.Button
+import android.widget.HorizontalScrollView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import androidx.cardview.widget.CardView
+import com.example.google_task.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var listsLayout : LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        listsLayout = findViewById(R.id.lists_layout)
     }
+
+    @SuppressLint("InflateParams")
+    public fun onClick_New_List(view: View){
+        val newButton = Button(this)
+        val layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        newButton.layoutParams = layoutParams
+        newButton.text = "custom name"
+
+        listsLayout.addView(newButton)
+    }
+
+
 }
